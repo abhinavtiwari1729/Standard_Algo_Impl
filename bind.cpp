df2a6408a2f11258af85fa1475ce574ee88951c0
Abhinav Tiwari <abhinavtiwari1729@gmail.com>
2024-01-27 00:54:51 +0530

    adding std bind example

---
+++ bind.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<functional>
+
+using namespace std;
+
+int multiply(int a , int b) {
+
+    return a*b;
+}
+
+struct entity{
+
+    int a ;
+
+    entity(int b): a(b) {
+
+    }
+
+    void func(int b){
+        cout << a*b << endl;
+    }
+
+};
+
+
+int main(){
+
+    auto multiply_10 = bind(multiply , 10 , placeholders::_1);
+    //cout << multiply_10(5);
+
+    entity E(10);
+    auto print_f = bind(entity::func , E , placeholders::_1);
+    print_f(2);
+
+
+}
