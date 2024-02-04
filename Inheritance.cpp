214635b60a0408c689154f460de5157a941b1822
Abhinav Tiwari <abhinavtiwari1729@gmail.com>
2024-02-04 19:11:52 +0530

    Inheritence example

---
+++ Inheritance.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+class Entity{
+
+    public:
+    Entity(){
+        cout << "Entity constructor" << endl;
+    }
+
+    ~Entity(){
+        cout << "Entity destructor" << endl;
+    }
+
+}; 
+class Monster : public Entity{
+
+    public :
+    Monster(){
+        cout << "Monster constructor" << endl;
+    }
+
+    ~Monster(){
+        cout << "Monster destructor" << endl;
+    }
+
+};
+int main(){
+    Monster m1;
+
+}
