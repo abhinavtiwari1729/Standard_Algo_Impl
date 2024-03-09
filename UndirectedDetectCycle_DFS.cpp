4ace25813776fb8bb5dbdf7e41e68bc4e3eecace
Abhinav Tiwari <abhinavtiwari1729@gmail.com>
2024-03-09 21:08:49 +0530

    Detect a cycle in a undirected graph (using dfs)

---
+++ UndirectedDetectCycle_DFS.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+bool isCyclicUtil(int node , vector<bool> &visited , vector<int> adj[] , int parent){
+
+    visited[node] = true;
+
+    for(auto adj_node : adj[node]){
+
+        if(!visited[adj_node])
+            return isCyclicUtil(adj_node , visited , adj , node);
+
+        else if(parent != node)
+            return true;
+    }
+
+    return false;
+}
+
+
+bool isCycle(int V , vector<int> adj[]){
+
+    vector<bool> visited(V , false);
+    for(int i = 0 ; i < V ; i++){
+        if(!visited[i])
+            if(isCyclicUtil(i , visited , adj, -1))
+                return true;
+
+    }
+
+    return false;
+}
+
+
+
+int main(){
+
+
+}
